985a690de03435a613c21044d99b3574384e7c1e
Geanta Adrian <geanta300@gmail.com>
2023-07-25 20:35:13 +0300

    database complete

--- app/src/main/java/com/example/counterreader/MainActivity.java
+++ app/src/main/java/com/example/counterreader/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     Button makePhoto, saveButton;
 
     DatabaseHelper myDB;
-    Boolean firstTimeDB;
 
     SharedPreferences sharedPref;
     String scannedQRCode;
@@ -38,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         sharedPref = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        firstTimeDB = sharedPref.getBoolean("firstTimeDB", false);
         scannedQRCode = sharedPref.getString("scannedQRCode", scannedQRCode);
 
         imageURI = getIntent().getStringExtra("imagePath");
@@ -51,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
         saveButton = findViewById(R.id.saveButton);
 
         myDB = new DatabaseHelper(MainActivity.this);
-        createInitialDatabase();
 
         if (imageURI != null) {
             imageView.setImageURI(Uri.parse(imageURI));
@@ -157,40 +154,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
         alertDialog.show();
-    }
-
-    public void createInitialDatabase() {
-        if (!firstTimeDB) {
-            myDB.insertData("CEC",                      "agentie",  "termic rece",      "72089956",             34.920,     "100001");
-            myDB.insertData("CEC",                      "agentie",  "termic cald",      "72083110",             10.100,     "100002");
-            myDB.insertData("CEC",                      "2",        "server termic",    "717661501 ET 2",       28.384,     "100003");
-            myDB.insertData("CEC",                      "3",        "server termic",    "71706294 ET 3",        19.381,     "100004");
-            myDB.insertData("CEC",                      "4",        "server termic",    "71706293 ET 4",        20.581,     "100005");
-            myDB.insertData("CEC",                      "ET 4",     "apa",              "46004593",             679.152,    "100006");
-            myDB.insertData("CEC",                      "ET 4",     "apa",              "46004614",             198.404,    "100007");
-            myDB.insertData("Tabac",                    "parter",   "termic rece",      "71761498",             18.377,     "100008");
-            myDB.insertData("Tabac",                    "parter",   "termic cald",      "71761497",             5.329,      "100009");
-            myDB.insertData("Ted's",                    "parter",   "termic rece",      "71761500",             33.173,     "100010");
-            myDB.insertData("Ted's",                    "parter",   "termic cald",      "71761499",             24.197,     "100011");
-            myDB.insertData("Ted's",                    "parter",   "apa",              "",                     439.354,    "100012");
-            myDB.insertData("Roche",                    "15",       "server",           "71925161",             55.722,     "100013");
-            myDB.insertData("Roche",                    "16",       "server",           "71925152",             53.900,     "100014");
-            myDB.insertData("BAI SEVICIU",              "25",       "apa",              "",                     74.863,     "100015");
-            myDB.insertData("Parcare",                  "S1",       "electric",         "1019304131",           4212.100,   "100016");
-            myDB.insertData("Parcare",                  "S1",       "electric",         "1019304097",           137014.200, "100017");
-            myDB.insertData("Parcare",                  "S1",       "electric",         "1020115119",           3719.000,   "100018");
-            myDB.insertData("Parcare pwc",              "",         "electric",         "121177079",            9218.500,   "100019");
-            myDB.insertData("Parcare",                  "S1",       "electric",         "1020115112",           5.329,      "100020");
-            myDB.insertData("Parcare",                  "S2",       "electric",         "1019464163",           5.329,      "100021");
-            myDB.insertData("Spalatorie ATO",           "S3",       "electric",         "26-33010",             5.329,      "100022");
-            myDB.insertData("Irigatii parcare 1",       "S1",       "apa",              "8ZRI1914765533",       5.329,      "100023");
-            myDB.insertData("Irigatii parcare 2",       "S1",       "apa",              "8ZRI1915032202",       5.329,      "100024");
-            myDB.insertData("Irigatii curte lumina",    "S2",       "apa",              "8ZRI1915032208",       5.329,      "100025");
-            myDB.insertData("Vodafone",                 "S1",       "electric",         "190100000662/P34S02",  30496.000,  "100026");
-
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putBoolean("firstTimeDB", true);
-            editor.apply();
-        }
     }
 }
--- app/src/main/java/com/example/counterreader/QRScan.java
+++ app/src/main/java/com/example/counterreader/QRScan.java
@@ -30,7 +30,9 @@ public class QRScan extends AppCompatActivity implements ZXingScannerView.Result
 
     ImageView flashButton;
     Button backToExport;
-    boolean backToExportBoolean;
+    boolean backToExportBoolean = false;
+    Boolean firstTimeDB;
+    DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,7 @@ public class QRScan extends AppCompatActivity implements ZXingScannerView.Result
         setContentView(R.layout.qr_scan_activity);
 
         sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        firstTimeDB = sharedPreferences.getBoolean("firstTimeDB", false);
         backToExportBoolean = sharedPreferences.getBoolean("editData",backToExportBoolean);
 
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
@@ -51,7 +54,8 @@ public class QRScan extends AppCompatActivity implements ZXingScannerView.Result
             scannerView.toggleFlash();
         });
 
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        databaseHelper = new DatabaseHelper(this);
+        createInitialDatabase();
         int counters = databaseHelper.getIndexesHigherThanZero();
         int maxCounters= databaseHelper.getRowCount();
         if (counters == maxCounters){
@@ -133,6 +137,64 @@ public class QRScan extends AppCompatActivity implements ZXingScannerView.Result
             } else {
                 Toast.makeText(this, "Camera permission needed", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    public void createInitialDatabase() {
+        if (!firstTimeDB) {
+            databaseHelper.insertData("CEC",                      "agentie",          "termic rece",      "72089956",             34.920,     "100001");
+            databaseHelper.insertData("CEC",                      "agentie",          "termic cald",      "72083110",             10.100,     "100002");
+            databaseHelper.insertData("CEC",                      "2",                "server termic",    "717661501 ET 2",       28.384,     "100003");
+            databaseHelper.insertData("CEC",                      "3",                "server termic",    "71706294 ET 3",        19.381,     "100004");
+            databaseHelper.insertData("CEC",                      "4",                "server termic",    "71706293 ET 4",        20.581,     "100005");
+            databaseHelper.insertData("CEC",                      "ET 4",             "apa",              "46004593",             679.152,    "100006");
+            databaseHelper.insertData("CEC",                      "ET 4",             "apa",              "46004614",             198.404,    "100007");
+            databaseHelper.insertData("Tabac",                    "parter",           "termic rece",      "71761498",             18.377,     "100008");
+            databaseHelper.insertData("Tabac",                    "parter",           "termic cald",      "71761497",             5.329,      "100009");
+            databaseHelper.insertData("Ted's",                    "parter",           "termic rece",      "71761500",             33.173,     "100010");
+            databaseHelper.insertData("Ted's",                    "parter",           "termic cald",      "71761499",             24.197,     "100011");
+            databaseHelper.insertData("Ted's",                    "parter",           "apa",              "",                     439.354,    "100012");
+            databaseHelper.insertData("Roche",                    "15",               "server",           "71925161",             55.722,     "100013");
+            databaseHelper.insertData("Roche",                    "16",               "server",           "71925152",             53.900,     "100014");
+            databaseHelper.insertData("BAI SEVICIU",              "25",               "apa",              "",                     74.863,     "100015");
+            databaseHelper.insertData("Parcare",                  "S1",               "electric",         "1019304131",           4212.100,   "100016");
+            databaseHelper.insertData("Parcare",                  "S1",               "electric",         "1019304097",           137014.200, "100017");
+            databaseHelper.insertData("Parcare",                  "S1",               "electric",         "1020115119",           3719.000,   "100018");
+            databaseHelper.insertData("Parcare pwc",              "",                 "electric",         "121177079",            9218.500,   "100019");
+            databaseHelper.insertData("Parcare",                  "S1",               "electric",         "1020115112",           85056.100,  "100020");
+            databaseHelper.insertData("Parcare",                  "S2",               "electric",         "1019464163",           68722.100,  "100021");
+            databaseHelper.insertData("Spalatorie ATO",           "S3",               "electric",         "26-33010",             13162.910,  "100022");
+            databaseHelper.insertData("Irigatii parcare 1",       "S1",               "apa",              "8ZRI1914765533",       8241.000,   "100023");
+            databaseHelper.insertData("Irigatii parcare 2",       "S1",               "apa",              "8ZRI1915032202",       1682.000,   "100024");
+            databaseHelper.insertData("Irigatii curte lumina",    "S2",               "apa",              "8ZRI1915032208",       40.000,     "100025");
+            databaseHelper.insertData("Vodafone",                 "S1",               "electric",         "190100000662/P34S02",  30496.000,  "100026");
+            databaseHelper.insertData("orange",                   "S3",               "electric",         "0120/SGS0217",         28556.520,  "100027");
+            databaseHelper.insertData("MSD server",               "5",                "termic rece",      "71706295",             37.972,     "100028");
+            databaseHelper.insertData("Contor parter baie",       "parter",           "apa",              "50007705",             172.487,    "100029");
+            databaseHelper.insertData("AV8 Restaurant",           "Et 25",            "gaz",              "5551009/2020",         2788.477,   "100030");
+            databaseHelper.insertData("Roche",                    "parter",           "apa",              "39800961",             15.990,     "100031");
+            databaseHelper.insertData("Roche",                    "parter",           "termic rece",      "71925154",             0.580,      "100032");
+            databaseHelper.insertData("Roche",                    "parter",           "termic rece",      "71925157",             34.644,     "100033");
+            databaseHelper.insertData("Roche",                    "parter",           "termic rece",      "71925158",             4.967,      "100034");
+            databaseHelper.insertData("Roche",                    "parter",           "termic cald",      "71925153",             48.616,     "100035");
+            databaseHelper.insertData("Roche",                    "parter",           "termic cald",      "71925160",             48.117,     "100036");
+            databaseHelper.insertData("Roche",                    "parter",           "termic cald",      "71925159",             25.418,     "100037");
+            databaseHelper.insertData("P29 ato",                  "parter",           "apa",              "49008663",             18.794,     "100038");
+            databaseHelper.insertData("P29 ato",                  "parter",           "termic cald",      "71925156",             2.551,      "100039");
+            databaseHelper.insertData("P29 ato",                  "parter",           "termic rece",      "71925155",             5.466,      "100040");
+            databaseHelper.insertData("vestiare mentenanta",      "",                 "apa",              "EZRI0250007721",       202.684,    "100041");
+            databaseHelper.insertData("vestiar PWC",              "S2",               "electric",         "",                     2720.410,   "100042");
+            databaseHelper.insertData("Contor -2 MSD",            "S2",               "electric",         "",                     0,          "100043");
+            databaseHelper.insertData("Contor -3 Roche",          "S3",               "electric",         "521236105",            1125,       "100044");
+            databaseHelper.insertData("Contor 26 MSD",            "26",               "electric",         "DDS6788",              789.6,      "100045");
+            databaseHelper.insertData("Contor E-infra",           "S2",               "electric",         "EN50470-3",            864.75,     "100046");
+            databaseHelper.insertData("Contor general de gaz",    "mecanic [m3]",     "gaz",              "3403401178/2017",      380795.92,  "100047");
+            databaseHelper.insertData("Contor general de gaz",    "electronic [m3]",  "gaz",              "Corus / Itron",        380846.1,   "100048");
+            databaseHelper.insertData("Contor general de gaz",    "convertit [Nm3]",  "gaz",              "Corus / Itron",        455644.053, "100049");
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("firstTimeDB", true);
+            editor.apply();
         }
     }
 }
